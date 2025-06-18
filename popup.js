970f1b761ce6975b4809693defc4b0a2e7148601
JasonVBN <jason.vbnguyen@gmail.com>
2025-06-18 13:51:30 -0500

    ability to see & close tabs

---
+++ popup.js
@@ -0,0 +1,24 @@
+document.getElementById('clickMe').addEventListener('click', () => {
+    chrome.tabs.query({}, function(tabs) {
+        const titlesList = document.createElement('ul');
+        tabs.forEach(tab => {
+            const li = document.createElement('li');
+            
+            
+            const close_btn = document.createElement('button');
+            close_btn.textContent = 'Close';
+            close_btn.addEventListener('click', () => {
+                chrome.tabs.remove(tab.id);
+                li.remove(); // Remove list item from the popup
+            });
+            li.textContent = `${tab.title} `;
+            li.appendChild(close_btn);
+            
+            titlesList.appendChild(li);
+
+        });
+        const tabInfo_div = document.getElementById('tabInfo');
+        tabInfo_div.textContent = `Open tabs: ${tabs.length}`; //clears old content
+        tabInfo_div.appendChild(titlesList);
+    });
+});
